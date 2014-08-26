50e27438e781510090d3edac3064017e43e7df2b
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-08-26 23:26:38 +0200

    Add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  get '/auth/:provider/callback', to: 'sessions#create'
+
   resources :friendships
 
   root 'friendships#index'
