ec06387436564ca9530244cb1c48dd0a000b3c66
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-08-27 00:07:48 +0200

    Fix user creation with Twitter credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env['omniauth.auth']
-    @user = User.find_or_create_by(provider: auth['provider'], uid: auth['uid']) do |user|
-      user.name = auth['info']['name']
+    @user = User.find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+      user.name = auth.info.name
     end
     session[:user_id] = @user.id
     redirect_to root_url
