a850aaed4e12d68e8dce8ea95b85c35eb3bd6155
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-08-27 01:00:07 +0200

    Add a flash helper to show flash contents

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def flash_bootstrap_class(type)
+    case type
+    when 'notice' then 'alert alert-info'
+    when 'alert' then 'alert alert-warning'
+    end
+  end
+
+  def flash_helper
+    flash_blocks = flash.collect do |type, message|
+      content_tag(:div, message, class: flash_bootstrap_class(type))
+    end
+    flash_blocks.join.html_safe
+  end
 end
