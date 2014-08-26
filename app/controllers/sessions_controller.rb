b82fd6a3d70d3f5d486926a8da5b5da7ba2c5771
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-08-26 23:51:07 +0200

    Implement create action for Omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    auth = request.env['omniauth.auth']
+    @user = User.find_or_create_by(provider: auth['provider'], uid: auth['uid']) do |user|
+      user.name = auth['info']['name']
+    end
+    session[:user_id] = @user.id
+    redirect_to root_url
   end
 
   def destroy
