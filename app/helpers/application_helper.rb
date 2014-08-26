892caf053907beeadf6be43ab01d79bf2cb2dedd
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-08-27 01:22:35 +0200

    Allow the user to close the flash alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,17 @@
 module ApplicationHelper
   def flash_bootstrap_class(type)
-    case type
-    when 'notice' then 'alert alert-info'
-    when 'alert' then 'alert alert-warning'
-    end
+    class_name = case type
+      when 'notice' then 'info'
+      when 'alert' then 'warning'
+      end
+    "alert alert-#{class_name} alert-dismissible"
   end
 
   def flash_helper
     flash_blocks = flash.collect do |type, message|
-      content_tag(:div, message, class: flash_bootstrap_class(type))
+      content_tag(:div, class: flash_bootstrap_class(type)) do
+        content_tag(:button, '&times;'.html_safe, data: { dismiss: 'alert' }, class: 'close') + message
+      end
     end
     flash_blocks.join.html_safe
   end
