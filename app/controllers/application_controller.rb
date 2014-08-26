5356350c940b71e8935b0115da8c7f5c2e0835e3
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-08-27 01:38:20 +0200

    Hide navbar components if the user is not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user
+
   def require_login
     unless logged_in?
       redirect_to login_url, alert: 'Only logged in users can access this page.'
