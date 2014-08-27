9c35fc413d62220a878c93456d702c2f58e105ad
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-08-28 00:23:10 +0200

    Create unit tests for User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user with invalid attributes" do
+    invalid_user = User.new
+    assert_not invalid_user.save
+  end
+
+  test "should save user with valid attributes" do
+    user = users(:valid_user)
+    assert user.save
+  end
 end
