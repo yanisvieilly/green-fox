bced7bb184d3a3a9016314c871bf4b9d37e30ed4
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-08-27 23:34:41 +0200

    Implement create action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,24 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i(edit update)
+
   def edit
-    @user = User.find(params[:id])
+
   end
+
+  def update
+    if @user.update(user_params)
+      redirect_to({ action: :edit }, notice: 'Profile successfully updated!')
+    else
+      render :edit, alert: 'Profile could not be updated.'
+    end
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :nickname, :location, :description)
+    end
 end
