8405585676bee7da9d7f963c31d06418e5baf01b
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-08-29 00:15:13 +0200

    Test user nickname uniqueness

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,4 +10,9 @@ class UserTest < ActiveSupport::TestCase
     user = users(:john_doe)
     assert user.save
   end
+
+  test "should not save user with an existing nickname" do
+    invalid_user = User.new(nickname: users(:john_doe).nickname, provider: 'twitter', uid: 777777)
+    assert_not invalid_user.save
+  end
 end
