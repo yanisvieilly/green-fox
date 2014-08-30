ff3eb954f5900ddd20425706e6be25faf7267fc2
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-08-30 22:44:47 +0200

    Link requests to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships, after_add: :add_inverse_friendship, after_remove: :remove_inverse_friendship
 
+  has_many :requests, foreign_key: :requester_id
+  has_many :inverse_requests, class_name: 'Request', foreign_key: :requested_id
+
   validates :uid, :provider, :nickname, presence: true
   validates :nickname, uniqueness: true
 
