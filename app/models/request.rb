1e377af0b6f8c20b5a7c314583d708c8fc938ea3
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-08-30 22:48:20 +0200

    Implement friendship request accept

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,8 @@
 class Request < ActiveRecord::Base
   belongs_to :requester, class_name: 'User'
   belongs_to :requested, class_name: 'User'
+
+  def accept
+    requester.friends << requested
+  end
 end
