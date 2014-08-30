dec0fcca743f1ad39d52a1ea41eba8b5e42bce60
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-08-30 22:20:22 +0200

    Add two-way friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ActiveRecord::Base
   has_many :friendships, dependent: :destroy
-  has_many :friends, through: :friendships
+  has_many :friends, through: :friendships, after_add: :add_inverse_friendship, after_remove: :remove_inverse_friendship
 
   validates :uid, :provider, :nickname, presence: true
   validates :nickname, uniqueness: true
+
+  private
+    def add_inverse_friendship(friend)
+      friend.friends << self unless friend.friends.include?(self)
+    end
+
+    def remove_inverse_friendship(friend)
+      friend.friends.delete(self) if friend.friends.include?(self)
+    end
 end
