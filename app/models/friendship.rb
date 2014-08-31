f744e490a895f6fa7f6ec870005e46bf4d9e5f67
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-08-31 15:11:46 +0200

    Refactor friendship management model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,16 @@
 class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  after_create :add_inverse_friendship
+  after_destroy :remove_inverse_friendship
+
+  private
+    def add_inverse_friendship
+      Friendship.find_or_create_by(user_id: self.friend_id, friend_id: self.user_id)
+    end
+
+    def remove_inverse_friendship
+      Friendship.find_by(user_id: self.friend_id, friend_id: self.user_id).try(:destroy)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,10 @@
 class User < ActiveRecord::Base
   has_many :friendships, dependent: :destroy
-  has_many :friends, through: :friendships, after_add: :add_inverse_friendship, after_remove: :remove_inverse_friendship
+  has_many :friends, through: :friendships
 
   has_many :requests, foreign_key: :requester_id
   has_many :inverse_requests, class_name: 'Request', foreign_key: :requested_id
 
   validates :uid, :provider, :nickname, presence: true
   validates :nickname, uniqueness: true
-
-  private
-    def add_inverse_friendship(friend)
-      friend.friends << self unless friend.friends.include?(self)
-    end
-
-    def remove_inverse_friendship(friend)
-      friend.friends.delete(self) if friend.friends.include?(self)
-    end
 end
