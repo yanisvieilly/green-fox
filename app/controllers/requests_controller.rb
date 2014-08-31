dc1e618094700eb5b746198a75c357544625f48f
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-08-31 02:30:26 +0200

    Add create action for request controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,6 +1,11 @@
 class RequestsController < ApplicationController
   before_action :set_request, only: %i(update destroy)
 
+  def create
+    @request = Request.create(requester_id: params[:requester_id], requested_id: params[:requested_id])
+    render nothing: true
+  end
+
   def update
     @request.accept
     render json: @request
