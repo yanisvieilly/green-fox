d80aa0d9331ee662e7255c811bc6a8195a8edc0b
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-08-31 22:33:53 +0200

    Add friendship controller tests

--- test/controllers/friendships_controller_test.rb
+++ test/controllers/friendships_controller_test.rb
@@ -1,5 +1,27 @@
 require 'test_helper'
 
 class FriendshipsControllerTest < ActionController::TestCase
+  def setup
+    @user = users(:john_doe)
+    session[:user_id] = @user.id
+  end
 
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should not get index when not logged in" do
+    session[:user_id] = nil
+    get :index
+    assert_redirected_to login_url
+    assert_equal 'Only logged in users can access this page.', flash[:alert]
+  end
+
+  test "should destroy friendship" do
+    assert_difference('Friendship.count', -2) do
+      delete :destroy, { id: friendships(:three).id }
+    end
+    assert_response :success
+  end
 end
